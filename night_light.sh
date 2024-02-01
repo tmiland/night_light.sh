d378dfdfb0c60c2afa5adc6ffe7391dfa92893ec
Tommy Miland <kontakt@tmiland.com>
2024-02-01 18:44:52 +0100

    Update night_light.sh

--- night_light.sh
+++ night_light.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+# shellcheck disable=SC2004,SC2317,SC2053
 
 ## Author: Tommy Miland (@tmiland) - Copyright (c) 2023
 
@@ -11,7 +11,7 @@
 ####                   Maintained by @tmiland                     ####
 ######################################################################
 
-VERSION='1.0.0' # Must stay on line 14 for updater to fetch the numbers
+# VERSION='1.0.0' # Must stay on line 14 for updater to fetch the numbers
 
 #------------------------------------------------------------------------------#
 #
@@ -39,16 +39,47 @@ VERSION='1.0.0' # Must stay on line 14 for updater to fetch the numbers
 #
 #------------------------------------------------------------------------------#
 ## Uncomment for debugging purpose
-# set -o errexit
-# set -o pipefail
-# set -o nounset
-# set -o xtrace
-
+if [[ $2 == "debug" ]]; then
+  set -o errexit
+  set -o pipefail
+  set -o nounset
+  set -o xtrace
+fi
 # Symlink: ln -sfn ~/.scripts/night_light.sh ~/.local/bin/night_light.sh
 # Crontab: 1 1 * * * bash ~/.scripts/night_light.sh > /dev/null 2>&1
 # Based on source: https://discussion.fedoraproject.org/t/can-i-manipulate-night-mode-from-command-line/72853/2
 
-CLOCK=24
+yr="1-68562/Norway/Telemark/Tinn/Rjukan"
+yr_url=https://www.yr.no/en/other-conditions/$yr
+
+pkg=lynx
+
+if ! dpkg -s $pkg >/dev/null 2>&1; then
+  apt install $pkg
+fi
+
+sun() {
+  $pkg --dump $yr_url | grep -oE "Sun$1 [[:digit:]]+:[[:digit:]]+" | sed -n "s/.*Sun$1 *\([^ ]*.*\)/\1/p"
+}
+
+sunrise=$(sun rise)
+sunset=$(sun set)
+
+sunrise-transition() {
+  date -d"$1$2 minutes $sunrise" '+%H:%M'
+}
+
+sunset-transition() {
+  date -d"$1$2 minutes $sunset" '+%H:%M'
+}
+
+cfg_file=/home/tommy/.github/night_light/.night_light_config
+# Read hidden configuration file with entries separated by " " into array
+IFS=' ' read -ra CfgArr < $cfg_file
+max_bright="${CfgArr[0]}"
+after_sunrise="${CfgArr[1]}"
+min_bright="${CfgArr[2]}"
+before_sunset="${CfgArr[3]}"
 
 # Source: https://www.omgubuntu.co.uk/2017/07/adjust-color-temperature-gnome-night-light
 # 1000 — Lowest value (super warm/red)
@@ -56,224 +87,18 @@ CLOCK=24
 # 5500 — Balanced night light temperature
 # 6500 — Default night light off temperature
 # 10000 — Highest value (super cool/blue)
-temperature_morning="5500"
-temperature_noon="6500"
+temperature_morning="4500"
+temperature_noon="$max_bright"
 temperature_evening="3500"
-temperature_night="2500"
+temperature_night="$min_bright"
 
-usage() {
-  # shellcheck disable=SC2046
-  printf "Usage: %s %s [options]\\n" "${CYAN}" $(basename "$0")
-  echo
-  echo "  If called without arguments, uses 24 hour clock."
-  echo
-  printf "  --24hour            | -24           use 24 hour clock\\n"
-  printf "  --12hour            | -12           use 12 hour clock\\n"
-  printf "  --light-enabled     | -le           turn on/off (true/false)\\n"
-  printf "  --light-temperature | -lt           show light-temperature\\n"
-  printf "  --dark-toggle       | -dt           toggle dark/light color scheme"
-  printf "\\n"
-  printf "  Crontab: 1 * * * * bash ~/.scripts/night_light.sh > /dev/null 2>&1\\n"
-  echo
+config() {
+  . ./night_light_config.sh
 }
 
-ARGS=()
-while [[ $# -gt 0 ]]; do
-  case $1 in
-    --help | -h)
-      usage
-      exit 0
-      ;;
-    --24hour | -24)
-      shift
-      CLOCK=24
-      ;;
-    --12hour | -12)
-      shift
-      CLOCK=12
-      ;;
-    --light-enabled | -le) # Bash Space-Separated (e.g., --option argument)
-      gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled "$2" # Source: https://stackoverflow.com/a/14203146
-      shift # past argument
-      shift # past value
-      ;;
-    --light-temperature | -lt)
-      gsettings get org.gnome.settings-daemon.plugins.color night-light-temperature
-      shift
-      ;;
-    --dark-toggle | -dt)
-      DT=1
-      shift
-      ;;
-    -*|--*)
-      printf "Unrecognized option: $1\\n\\n"
-      usage
-      exit 1
-      ;;
-    *)
-      ARGS+=("$1")
-      shift
-      ;;
-  esac
-done
-
-set -- "${ARGS[@]}"
-
-currentmonth=$(date +%m)
-
-if [[ $CLOCK = 24 ]]; then
-  currenttime=$(date +%H:%M)
-  case $currentmonth in
-    01 )
-      morning="09:00"
-      noon="12:00"
-      evening="15:00"
-      night="18:00"
-      ;;
-    02 )
-      morning="08:00"
-      noon="12:00"
-      evening="17:00"
-      night="20:00"
-      ;;
-    03 )
-      morning="07:00"
-      noon="12:00"
-      evening="18:00"
-      night="21:00"
-      ;;
-    04 )
-      morning="07:00"
-      noon="12:00"
-      evening="19:00"
-      night="21:00"
-      ;;
-    05 )
-      morning="07:00"
-      noon="12:00"
-      evening="20:00"
-      night="22:00"
-      ;;
-    06 )
-      morning="07:00"
-      noon="12:00"
-      evening="21:00"
-      night="23:00"
-      ;;
-    07 )
-      morning="07:00"
-      noon="12:00"
-      evening="20:00"
-      night="23:00"
-      ;;
-    08 )
-      morning="07:00"
-      noon="12:00"
-      evening="19:00"
-      night="23:00"
-      ;;
-    09 )
-      morning="08:00"
-      noon="12:00"
-      evening="18:00"
-      night="22:00"
-      ;;
-    10 )
-      morning="09:00"
-      noon="12:00"
-      evening="17:00"
-      night="21:00"
-      ;;
-    11 )
-      morning="10:00"
-      noon="12:00"
-      evening="16:00"
-      night="20:00"
-      ;;
-    12 )
-      morning="10:00"
-      noon="12:00"
-      evening="15:00"
-      night="20:00"
-      ;;
-  esac
-  # elif [[ $CLOCK = 12 ]]; then
-  #   currenttime=$(date +"%I:%M")
-  #   case $currentmonth in
-  #     01 )
-  #       morning="09:00 AM"
-  #       noon="12:00 PM"
-  #       evening="04:00 PM"
-  #       night="08:00 PM"
-  #       ;;
-  #     02 )
-  #       morning="08:00 AM"
-  #       noon="12:00 PM"
-  #       evening="05:00 PM"
-  #       night="09:00 PM"
-  #       ;;
-  #     03 )
-  #       morning="07:00 AM"
-  #       noon="12:00 PM"
-  #       evening="06:00 PM"
-  #       night="09:00 PM"
-  #       ;;
-  #     04 )
-  #       morning="07:00 AM"
-  #       noon="12:00 PM"
-  #       evening="07:00 PM"
-  #       night="09:00 PM"
-  #       ;;
-  #     05 )
-  #       morning="07:00 AM"
-  #       noon="12:00 PM"
-  #       evening="08:00 PM"
-  #       night="09:00 PM"
-  #       ;;
-  #     06 )
-  #       morning="07:00 AM"
-  #       noon="12:00 PM"
-  #       evening="10:00 PM"
-  #       night="10:00 PM"
-  #       ;;
-  #     07 )
-  #       morning="07:00 AM"
-  #       noon="12:00 PM"
-  #       evening="10:00 PM"
-  #       night="11:00 PM"
-  #       ;;
-  #     08 )
-  #       morning="07:00 AM"
-  #       noon="12:00 PM"
-  #       evening="09:00 PM"
-  #       night="11:00 PM"
-  #       ;;
-  #     09 )
-  #       morning="08:00 AM"
-  #       noon="12:00 PM"
-  #       evening="08:00 PM"
-  #       night="11:00 PM"
-  #       ;;
-  #     10 )
-  #       morning="09:00 AM"
-  #       noon="12:00 PM"
-  #       evening="07:00 PM"
-  #       night="10:00 PM"
-  #       ;;
-  #     11 )
-  #       morning="10:00 AM"
-  #       noon="12:00 PM"
-  #       evening="06:00 PM"
-  #       night="09:00 PM"
-  #       ;;
-  #     12 )
-  #       morning="10:00 AM"
-  #       noon="12:00 PM"
-  #       evening="05:00 PM"
-  #       night="08:00 PM"
-  #       ;;
-  #   esac
-fi
+night-light-temperature() {
+  gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature "$1"
+}
 
 dark_toggle=$([[ $(gsettings get org.gnome.desktop.interface color-scheme) =~ "dark" ]] && echo dark || echo light)
 
@@ -309,38 +134,190 @@ toggle_light() {
   fi
 }
 
-night_light() {
+# Source: https://askubuntu.com/a/894470
+# global variable
+LastSetting=$(gsettings get org.gnome.settings-daemon.plugins.color night-light-temperature | sed 's|uint32 ||g')
 
-  if [ -n "${1}" ]; then
-    gsettings set \
-      org.gnome.settings-daemon.plugins.color \
-      night-light-temperature "${1}"
+auto-run() {
+  while true
+  do
+
+    # Current seconds
+    secNow=$(date +"%s")
+    secSunrise=$(date --date="$sunrise today" +%s)
+    secSunset=$(date --date="$sunset today" +%s)
+
+    set-and-sleep() {
+      if [[ $1 != $LastSetting ]]
+      then
+        # sudo sh -c "echo $1 | sudo tee $backlight"
+        # echo "$1" > "/tmp/display-current-brightness"
+        LastSetting="$1"
+        night-light-temperature "$1"
+        echo "Temperature set to ($1)"
+      fi
+      sleep 60
+    }
+
+    re='^[0-9]+$'   # regex for valid numbers
+
+    calc-level-and-sleep() {
+      # Parms $1 = number of minutes for total transition
+      #       $2 = number of seconds into transition
+      # Daytime - nightime = transition brightness levels
+      transition_spread=$(( $max_bright - $min_bright ))
+      secTotal=$(( $1 * 60 )) # Convert total transition minutes to seconds
+      Adjust=$( bc <<< "scale=6; $transition_spread * ( $2 / $secTotal )" )
+      Adjust=$( echo "$Adjust" | cut -f1 -d"." ) # Truncate number to integer
+
+      if ! [[ $Adjust =~ $re ]] ; then
+        Adjust=0   # When we get to last minute $Adjust can be non-numeric
+      fi
+
+      calc_bright=$(( $min_bright + $Adjust ))
+      set-and-sleep "$calc_bright"
+    }
+    # We're somewhere between sunrise and sunset
+    secMaxCutoff=$(( $secSunrise + ( $after_sunrise * 60 ) ))
+    secMinStart=$((  $secSunset  - ( $before_sunset * 60 ) ))
+
+    # Are we between sunrise and full brightness?
+    if [[ "$secNow" -gt "$secSunrise" ]] && [[ "$secNow" -lt "$secMaxCutoff" ]]
+    then
+      # Current time - Sunrise = progress through transition
+      secPast=$(( $secNow - $secSunrise ))
+      calc-level-and-sleep "$after_sunrise" $secPast
+      PastDuration=$(date +%H:%M:%S -ud @${secPast})
+      echo "Transitioning $PastDuration minutes after sunrise (Currently set to: $after_sunrise)."
+      continue
+    fi
+    # Is it full bright day time?
+    if [[ "$secNow" -gt "$secMaxCutoff" ]] && [[ "$secNow" -lt "$secMinStart" ]]
+    then
+      # MAXIMUM: after sunrise transition AND before nightime transition
+      set-and-sleep "$max_bright"
+      continue
+    fi
+    # Are we between beginning to dim and sunset (full dim)?
+    if [[ "$secNow" -gt "$secMinStart" ]] && [[ "$secNow" -lt "$secSunset" ]]
+    then
+      # Sunset - Current time = progress through transition
+      secBefore=$(( $secSunset - $secNow ))
+      calc-level-and-sleep "$before_sunset" $secBefore
+      BeforeDuration=$(date +%H:%M:%S -ud @${secBefore})
+      echo "Transitioning $BeforeDuration minutes before sunset (Currently set to: $before_sunset)."
+      continue
+    fi
+    # Is it night time?
+    if [[ "$secNow" -gt "$secSunset" ]] || [[ "$secNow" -lt "$secSunrise" ]]
+    then
+      # MINIMUM: after sunset or before sunrise nightime setting
+      set-and-sleep "$min_bright"
+      toogle_dark
+      continue
+    fi
+    # At this stage brightness was set with manual override outside this program
+    # or exactly at a testpoint, then it will change next minute so no big deal.
+    sleep 60 # reset brightness once / minute.
+  done # End of forever loop
+}
+
+usage() {
+  # shellcheck disable=SC2046
+  printf "Usage: %s %s [options]\\n" "" $(basename "$0")
+  echo
+  echo "  If called without arguments, uses 24 hour clock."
+  echo
+  printf "  --24hour            | -24           use 24 hour clock\\n"
+  printf "  --12hour            | -12           use 12 hour clock\\n"
+  printf "  --light-enabled     | -le           turn on/off (true/false)\\n"
+  printf "  --light-temperature | -lt           show light-temperature\\n"
+  printf "  --dark-toggle       | -dt           toggle dark/light color scheme\\n"
+  printf "  --auto-run          | -ar           auto run\\n"
+  printf "  --config            | -c            run config dialog"
+  printf "\\n"
+  printf "  Crontab: 1 * * * * bash ~/.scripts/night_light.sh > /dev/null 2>&1\\n"
+  echo
+}
+
+DT=
+AR=
+
+ARGS=()
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --help | -h)
+      usage
+      exit 0
+      ;;
+    --light-enabled | -le) # Bash Space-Separated (e.g., --option argument)
+      gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled "$2" # Source: https://stackoverflow.com/a/14203146
+      shift # past argument
+      shift # past value
+      ;;
+    --light-temperature | -lt)
+      echo "$LastSetting"
+      exit 0
+      ;;
+    --dark-toggle | -dt)
+      DT=1
+      shift
+      ;;
+    --auto-run | -ar)
+      AR=1
+      auto-run
+      shift
+      ;;
+    --config | -c)
+      config
+      exit 0
+      ;;
+    -*|--*)
+      printf "Unrecognized option: $1\\n\\n"
+      usage
+      exit 1
+      ;;
+    *)
+      ARGS+=("$1")
+      shift
+      ;;
+  esac
+done
+
+set -- "${ARGS[@]}"
+
+currenttime=$(date +%H:%M)
+# morning="$sunrise"
+morning=$(sunrise-transition + "$after_sunrise")
+noon="12:00"
+evening=$(sunset-transition - "$before_sunset")
+night=$(sunset-transition + "$before_sunset")
+
+night_light() {
+  if ! [[ $AR == "1" ]] && [[ -n "${1}" ]]; then
+    night-light-temperature "${1}"
+    return
   elif [[ ! ( "$currenttime" < "$morning" || "$currenttime" > "$noon" ) ]]; then
-    gsettings set \
-      org.gnome.settings-daemon.plugins.color \
-      night-light-temperature $temperature_morning
+    night-light-temperature $temperature_morning
     echo "Temperature set to morning ($temperature_morning)"
+    return
     toggle_light
   elif [[ ! ( "$currenttime" < "$noon" || "$currenttime" > "$evening" ) ]]; then
-    gsettings set \
-      org.gnome.settings-daemon.plugins.color \
-      night-light-temperature $temperature_noon
+    night-light-temperature $temperature_noon
     echo "Temperature set to noon ($temperature_noon)"
+    return
     toggle_light
   elif [[ ! ( "$currenttime" < "$evening" || "$currenttime" > "$night" ) ]]; then
-    gsettings set \
-      org.gnome.settings-daemon.plugins.color \
-      night-light-temperature $temperature_evening
+    night-light-temperature $temperature_evening
     echo "Temperature set to evening ($temperature_evening)"
+    return
     toogle_dark
   elif [[ ! ( "$currenttime" < "$night" ) ]]; then
-    gsettings set \
-      org.gnome.settings-daemon.plugins.color \
-      night-light-temperature $temperature_night
+    night-light-temperature $temperature_night
     echo "Temperature set to night ($temperature_night)"
+    return
     toogle_dark
   fi
-
 }
 
 night_light "$@"
