5182702e8d3643bad90a754502d3d14db8ddc2ef
Tommy Miland <kontakt@tmiland.com>
2026-03-02 17:44:35 +0100

    Add brightness icon
    - Added brightness icon

--- night_light_sysmon.sh
+++ night_light_sysmon.sh
@@ -13,9 +13,9 @@
 
 if command -v gsettings get org.gnome.settings-daemon.plugins.color night-light-temperature >/dev/null 2>&1; then
   Brightness=$(gsettings get org.gnome.settings-daemon.plugins.color night-light-temperature | sed 's|uint32 ||g')
-  systray="$systray Brightness: $Brightness"
+  systray="🔆 $Brightness"
 else
-  systray="$systray Brightness: OFF"
+  systray="Brightness: OFF"
 fi
 
 echo "$systray" # sysmon-indidicator will put echo string into systray for us.
